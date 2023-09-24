ace2d6eb00771d163ad613553bc1b65bf9ea28d2
kannankdl35 <145252426+kannankdl35@users.noreply.github.com>
2023-09-24 20:15:48 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("API_ID", default=12099140, cast=int)
+API_HASH = config("API_HASH", default=1740646db2628dabd953712e45b044a1)
+BOT_TOKEN = config("BOT_TOKEN", default=6549391784:AAF630wJgDxp-YetbMOsWd8EBj8yDgaqusk)
+SESSION = config("SESSION", default=BQCxIhWVjtIsKpbWuvVeCABPEdGgMPNmPVmdc7FCl-kn5P4f0aTgVmuSdHL5IPkf7378ykmdBTbqLiFg_TnHS4jl-D6g2T50_VoQrdUVu4x2v-d2A_3Ii51W8rP1nXuo9UDXSBHhFEzW5uVyIEGD24VWBCIO5lM3S0TMDLPAfd4fbpXanvSeLila0Z8sc_kWpx-zzmld8Y15-KfbIBXBDlW5nV-kXg39FePy0SrI2nSRTTi1PXGtnj6neSdvK4nHZlLw1SmkFc4O5T1Gsdzg1N4W3Dm9CDxHjAuUXDo9IFgblIHb1SDaee4CqCsWXway6kASettDr9mABF8B2sQ_4iUaAAAAATgvoVIA)
+FORCESUB = config("FORCESUB", default=Pantham_Malayalam)
+AUTH = config("AUTH", default=None, cast=5237612882)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
